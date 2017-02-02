415a24670d9d85c679493aba97bdb253c3ac4a0f
Monu <prankum2100@berkeley.edu>
2017-02-02 15:42:14 -0800

    made info box all pretty, added user image from google to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129025607) do
+ActiveRecord::Schema.define(version: 20170202233933) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "code"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20170129025607) do
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
     t.string   "email"
+    t.string   "image"
   end
 
 end
