3eed9eae37c2e95b6a9ae4a48a98c1185b3c7e57
Monu <prankum2100@berkeley.edu>
2017-02-21 16:19:42 -0800

    adds class to calendar from when the next session is, not from the current day (small bug fixed)

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,11 @@
+require 'date'
+
 class Course < ApplicationRecord
   belongs_to :user
 
   def sync_with_google(cal)
     title_string = self.parse_title
-    start_time = self.time_start
-    end_time = self.time_end
+    start_time, end_time = self.time_range
 
     event = cal.create_event do |e|
       e.title = title_string
@@ -22,15 +23,37 @@ class Course < ApplicationRecord
     self.save!
   end
 
+  def date_of_next(day)
+    real_day = case day
+    when 'mo'
+      'monday'
+    when 'tu'
+      'tuesday'
+    when 'we'
+      'wednesday'
+    when 'th'
+      'thursday'
+    when 'fr'
+      'friday'
+    else
+      'monday'
+    end
+    date  = Date.parse(real_day)
+    delta = date >= Date.today ? 0 : 7
+    date + delta
+  end
+
   def parse_title
     self.code.to_s + ': ' + self.title
   end
 
-  def time_start
-    self.time.split('-')[0] + ":00 -0800"
+  def time_range
+    next_date = self.day.split(',').map { |day| date_of_next(day) }.min
+    my_start_time = self.time.split('-')[0].split(':')
+    my_end_time = self.time.split('-')[1].split(':')
+    start_range = Time.new(next_date.year, next_date.month, next_date.day, my_start_time[0].to_i, my_start_time[1].to_i, 0, "-08:00")
+    end_range = Time.new(next_date.year, next_date.month, next_date.day, my_end_time[0].to_i, my_end_time[1].to_i, 0, "-08:00")
+    [start_range, end_range]
   end
 
-  def time_end
-    self.time.split('-')[1] + ":00 -0800"
-  end
 end
