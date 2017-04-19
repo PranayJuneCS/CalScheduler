d308d193f86d9bfae92190aaec6641926b085695
Monu <prankum2100@berkeley.edu>
2017-04-19 12:57:03 -0700

    increased width of google calendar, default view of it

--- app/assets/javascripts/components/ClassOverview.js.jsx
+++ app/assets/javascripts/components/ClassOverview.js.jsx
@@ -69,25 +69,27 @@ class ClassOverview extends React.Component {
 
   showCalendar() {
     return {
-      __html: '<iframe id="cal-iframe" src="https://calendar.google.com/calendar/embed?title=Your%20Calendar&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23336666&amp;src=' + encodeURIComponent(this.props.user.email) + '&amp;color=%23691426&amp;ctz=America%2FLos_Angeles" style="border:solid 1px #777" height="600" frameborder="0" scrolling="yes"></iframe>'
+      __html: '<iframe id="cal-iframe" src="https://calendar.google.com/calendar/embed?mode=WEEK&amp;title=Your%20Calendar&amp;showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23336666&amp;src=' + encodeURIComponent(this.props.user.email) + '&amp;color=%23691426&amp;ctz=America%2FLos_Angeles" style="border:solid 1px #777" height="600" frameborder="0" scrolling="yes"></iframe>'
     }
   }
 
   render() {
     return (
-      <div className="content container">
-        <div className="header-container">
-          <a onClick={this.syncClass} data-position="right" data-delay="1000" data-tooltip="Sync With Google" className="btn-floating btn waves-effect waves-light left tooltipped">
-            <i className={this.loadingSync("fa-refresh")}></i>
-          </a>
-          <h4 style={{flex: 1}}>My Schedule</h4>
-          <a href="/choose" data-position="left" data-delay="1000" data-tooltip="Add Courses" className="btn-floating btn waves-effect waves-light right tooltipped">
-            <i className="material-icons">add</i>
-          </a>
+      <div>
+        <div className="content container">
+          <div className="header-container">
+            <a onClick={this.syncClass} data-position="right" data-delay="1000" data-tooltip="Sync With Google" className="btn-floating btn waves-effect waves-light left tooltipped">
+              <i className={this.loadingSync("fa-refresh")}></i>
+            </a>
+            <h4 style={{flex: 1}}>My Schedule</h4>
+            <a href="/choose" data-position="left" data-delay="1000" data-tooltip="Add Courses" className="btn-floating btn waves-effect waves-light right tooltipped">
+              <i className="material-icons">add</i>
+            </a>
+          </div>
+          <ul className="collapsible popout" data-collapsible="accordion">
+            {this.state.courses.map(this.createClassInfo)}
+          </ul>
         </div>
-        <ul className="collapsible popout" data-collapsible="accordion">
-          {this.state.courses.map(this.createClassInfo)}
-        </ul>
         <div className="animated fadeIn">
           <div dangerouslySetInnerHTML={this.showCalendar()} />
         </div>
