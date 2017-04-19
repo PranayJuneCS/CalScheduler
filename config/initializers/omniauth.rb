2355f2b25eedda38eb4ab087279d6dc08a40c668
Monu <prankum2100@berkeley.edu>
2017-04-19 12:45:54 -0700

    preparing for release: clean up, add select account for google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,6 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["google_app_id"], ENV["google_app_secret"], 
     { scope: 'userinfo.email, userinfo.profile, calendar',
       client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}},
-      skip_jwt: true
+      skip_jwt: true,
+      prompt: 'select_account'
     }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227222015) do
+ActiveRecord::Schema.define(version: 20170419193835) do
 
   create_table "codes", force: :cascade do |t|
     t.datetime "created_at",    null: false
